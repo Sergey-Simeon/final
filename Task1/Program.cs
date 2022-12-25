d92c47c2c9e1375d0560af282de7bfbe687953f7
Sergey <sergei.simeon@yandex.ru>
2022-12-25 19:47:15 +0300

    task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Write number 1: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Write number 2: ");
+        int m = int.Parse(Console.ReadLine());
+        if(n < m)
+        {
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        }
+        else if(m < n)
+        {
+            for (int i = m; i <= n; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        }
+        Console.ReadKey();
+    }
+}
